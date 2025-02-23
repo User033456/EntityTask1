2c7ac7cae0089fe5d42c34e8b95ad7d6812ec32e
Stepanskeleton <ytyrbs228@gmail.com>
2025-02-23 20:05:14 +1000

    NewMenu

--- WpfApp2/obj/Debug/net8.0-windows/WpfApp2.AssemblyInfo.cs
+++ WpfApp2/obj/Debug/net8.0-windows/WpfApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb35265befba1e319956301a46fce101f73ef359")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8019a9b0f6c0ea9192e74fc45f0f33aece5a18d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
